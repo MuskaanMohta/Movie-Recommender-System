b4c9634849f2b800a4e3bf5f658a529625457252
Muskaan Mohta <muskaanmohta21@gmail.com>
2026-01-03 14:19:13 +0530

    commit

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 import streamlit as st
 import pickle
+import joblib
 import pandas as pd
 from tmdbv3api import TMDb, Movie
 import requests
+from sklearn.metrics.pairwise import cosine_similarity
+
+
 tmdb=TMDb()
 tmdb.api_key="4dae242264c1766dd6c3a5bde7515a07"
 base_url="https://image.tmdb.org/t/p/w500/"
 
 movies_dict=pickle.load(open("movies_dictionary.pkl","rb"))
-similarity=pickle.load(open("similarity.pkl","rb"))
+#similarity=pickle.load(open("similarity.pkl","rb"))
+vectors = joblib.load("vectors.pkl")
 movies=pd.DataFrame(movies_dict)
 
 #function to fetch posters of movies
@@ -32,7 +37,7 @@ def fetch_poster(movie_id):
 #function to recommend 5 movies along with their posters
 def recommend(movie):
     movie_index=movies[movies['title']==movie].index[0]
-    dist=similarity[movie_index]
+    dist = cosine_similarity(vectors[movie_index].reshape(1, -1), vectors).flatten()
     recommend_movies=sorted(enumerate(dist.tolist()),reverse=True,key=lambda x:x[1])[1:6]
     movie_list=[]
     posters=[]
